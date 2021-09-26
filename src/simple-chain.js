7cd95a50de067ee28df57a309d747d9b82fe3f45
Trinyash <Trinyash@protonmail.com>
2021-09-26 03:05:53 +0000

    feat: add task solution 'Chain maker'

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,28 +1,41 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
-export default {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+
+class ChainMaker {
+    constructor() {
+        this.chain = []
+    }
+
+    getLength() {
+        return this.chain.length
+    }
+
+    addLink(value) {
+        this.chain.push(`( ${value} )`)
+        return this
+    }
+
+    removeLink(position) {
+        if (!(position > 0 && position < this.chain.length)) {
+            this.chain = []
+            throw new Error('You can\'t remove incorrect link!')
+        }
+        this.chain.splice(position - 1, 1)
+        return this
+    }
+
+    reverseChain() {
+        this.chain.reverse()
+        return this
+    }
+
+    finishChain() {
+        const result = this.chain.join('~~')
+        this.chain = []
+        return result
+    }
+}
+
+export default new ChainMaker()
