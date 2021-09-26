695f1a7d2d8ce835218272bc6cab60775bdffed5
Trinyash <Trinyash@protonmail.com>
2021-09-26 04:24:05 +0000

    feat: add task solution 'Extended repeater'

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,21 +1,43 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
+ *
  * repeater('STRING', { repeatTimes: 3, separator: '**', 
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+export default function repeater(str, options) {
+    const defaultSeparator = '+'
+    const defaultAdditionSeparator = '|'
+
+    str = `${str}`
+    options = options !== undefined ? options : {};
+    options.repeatTimes = options.repeatTimes === undefined ? 1 : options.repeatTimes
+    options.separator = options.separator === undefined ? defaultSeparator : options.separator
+    options.addition = `${options.addition}`
+    options.additionRepeatTimes = options.additionRepeatTimes === undefined ? 1 : options.additionRepeatTimes
+    options.additionSeparator = options.additionSeparator === undefined ? defaultAdditionSeparator : options.additionSeparator
+
+    const result = []
+    for (let i = 0; i < options.repeatTimes; i++) {
+        result.push(str)
+        for (let j = 0; j < options.additionRepeatTimes; j++) {
+            result.push(options.addition)
+            if (j < options.additionRepeatTimes - 1) {
+                result.push(options.additionSeparator)
+            }
+        }
+        if (i < options.repeatTimes - 1) {
+            result.push(options.separator)
+        }
+    }
+    return result.join('')
 }
